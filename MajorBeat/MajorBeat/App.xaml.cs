cf81e3048301c2e4ee1585afedab5008397e7f68
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2025-10-06 22:35:19 -0300

    Feat: chat page updated

--- MajorBeat/MajorBeat/App.xaml.cs
+++ MajorBeat/MajorBeat/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MajorBeat
+﻿using MajorBeat.Views.Users;
+
+namespace MajorBeat
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new ConversationView();
         }
     }
 }
