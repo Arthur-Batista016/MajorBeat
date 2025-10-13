556c7c170e1bfd28fd79b529fd63078581d3ed82
GabrielWhanderson <gabrielwcastro09@gmail.com>
2025-10-12 21:11:36 -0300

    feat: adicionados entries e pickers

--- MajorBeat/MajorBeat/Views/Hirers/CreateEventPage.xaml.cs
+++ MajorBeat/MajorBeat/Views/Hirers/CreateEventPage.xaml.cs
@@ -1,9 +1,22 @@
-namespace MajorBeat.Views.Hirers;
-
-public partial class CreateEventPage : ContentPage
+namespace MajorBeat.Views.Hirers
 {
-	public CreateEventPage()
-	{
-		InitializeComponent();
-	}
+    public partial class CreateEventPage : ContentPage
+    {
+        public CreateEventPage()
+        {
+            InitializeComponent();
+            EventDatePicker.MinimumDate = DateTime.Today;
+        }
+
+        private async void OnCategoryChosen(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            if (picker.SelectedIndex < 0) return;
+
+            // pega o texto do item (quando usamos Items)
+            var selected = picker.Items[picker.SelectedIndex];
+
+            await DisplayAlert("Categoria", $"Você escolheu: {selected}", "OK");
+        }
+    }
 }
