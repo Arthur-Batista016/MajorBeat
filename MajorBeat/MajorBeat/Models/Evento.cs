4e70933fa1f94123994692e6ed2dea16edd9b73a
MATHEUS VIEIRA <matheus.vieira137@etec.sp.gov.br>
2025-10-24 10:48:35 -0300

    feat: adicionado Models Mensagem e Evento

---
+++ MajorBeat/MajorBeat/Models/Evento.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using MajorBeat.Models.Enums;
+
+namespace MajorBeat.Models
+{
+    public class Evento
+    {
+        public long IdEvento { get; set; }
+
+        public string Nome { get; set; }
+
+        public TipoMusico TipoMusico { get; set; }
+        public StatusEvento Status { get; set; }
+        public TipoEvento TipoEvento { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public string Endereco { get; set; }
+
+        public ObservableCollection<byte[]> ImagemLocalEvento { get; set; } = new();
+
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFim { get; set; }
+
+        public string? Descricao { get; set; }
+        public string Titulo { get; set; }
+        public ObservableCollection<NomeInstrumento> NomeInstrumento { get; set; } = new();
+
+        public ObservableCollection<NomeGenero> NomeGenero { get; set; } = new();
+
+        public Musico Musico { get; set; }
+
+        public Contratante Contratante { get; set; }
+
+        public ObservableCollection<Avaliacao> Avaliacoes { get; set; } = new();
+    }
+}
