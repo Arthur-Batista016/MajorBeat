7372620f6db01fb1cb30d91f3d57d3890dd84780
MATHEUS VIEIRA <matheus.vieira137@etec.sp.gov.br>
2025-10-24 21:53:49 -0300

    feat: Feito os Services

---
+++ MajorBeat/MajorBeat/Services/Hirers/HirerProfilePageService.cs
@@ -0,0 +1,24 @@
+﻿using MajorBeat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajorBeat.Services.Hirers
+{
+    public class HirerProfilePageService : Request
+    {
+        private readonly Request _request;
+        private const string _baseUrl = "https://majorbeat-fzedc4ekbuaufncw.brazilsouth-01.azurewebsites.net/Musico";
+        private string _token;
+        public string Token => _token;
+
+        public async Task<Contratante> GetContratanteByIdAsync(int id)
+        {
+            string urlComplementar = $"/getContratante/{id}";
+            Contratante contratante = await _request.GetAsync<Models.Contratante>(_baseUrl + urlComplementar);
+            return contratante;
+        }
+    }
+}
---
+++ MajorBeat/MajorBeat/Services/Musician/MusicianProfilePageService.cs
@@ -0,0 +1,25 @@
+﻿using MajorBeat.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajorBeat.Services.Musician
+{
+    public class MusicianProfilePageService : Request
+    {
+        private readonly Request _request;
+        private const string _baseUrl = "https://majorbeat-fzedc4ekbuaufncw.brazilsouth-01.azurewebsites.net/Musico";
+        private string _token;
+        public string Token => _token;
+
+        public async Task<Musico> GetMusicianByIdAsync(int id)
+        {
+            string urlComplementar = $"/getMusico/{id}";
+            Musico musico = await _request.GetAsync<Models.Musico>(_baseUrl + urlComplementar);
+            return musico;
+        }
+    }
+}
